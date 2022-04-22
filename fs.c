e2652b441343737594a487542fa3fb6e62c2231a
Corey Wurl <cwurl@nd.edu>
2022-04-21 22:55:13 -0400

    more stuff

--- fs.c
+++ fs.c
@@ -9,11 +9,13 @@ Make your changes here.
 #include <time.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <stdbool.h>
+#include <stdlib.h>
 
 extern struct disk *thedisk;
 
 #define FS_MAGIC           0x30341003
-#define INODES_PER_BLOCK   128
+#define INODES_PER_BLOCK   4
 #define POINTERS_PER_INODE 3
 #define POINTERS_PER_BLOCK 1024
 
@@ -39,25 +41,26 @@ union fs_block {
 	unsigned char data[BLOCK_SIZE];
 };
 
-int *freeBlocks;
+bool *freeBlocks;
+bool mounted = false;
 
 
 int fs_format()
 {
+	// Check if a filesystem is already mounted
+	if (mounted) {
+		return 0;
+	}
+
 	int i;
 	union fs_block superBlock;
 	disk_read(thedisk,0,superBlock.data);
 
-	// Check if a filesystem is already mounted
-	if (superBlock.super.magic == FS_MAGIC) {
-		return 0;
-	}
-
 	// Set aside 10% of blocks for inode blocks
-	if (thedisk->nblocks % 10 == 0) {
-		superBlock.super.ninodeblocks = (thedisk->nblocks / 10);
+	if (disk_nblocks(thedisk) % 10 == 0) {
+		superBlock.super.ninodeblocks = (disk_nblocks(thedisk) / 10);
 	} else {
-		superBlock.super.ninodeblocks = (thedisk->nblocks / 10) + 1;
+		superBlock.super.ninodeblocks = (disk_nblocks(thedisk) / 10) + 1;
 	}
 
 	// Clear the inode table (initializes data to random values)
@@ -65,17 +68,17 @@ int fs_format()
 	for (i = 0; i < BLOCK_SIZE; i++) {
 		randomBlock.data[i] = 0;
 	}
-	for (i = 1; i < thedisk->nblocks; i++) {
+	for (i = 1; i < disk_nblocks(thedisk); i++) {
 		disk_write(thedisk, i, randomBlock.data);
 	}
 
 	// Write the superblock
 	superBlock.super.magic = FS_MAGIC;
-	superBlock.super.nblocks = thedisk->nblocks;
+	superBlock.super.nblocks = disk_nblocks(thedisk);
 	superBlock.super.ninodes = INODES_PER_BLOCK * superBlock.super.ninodeblocks;
 	disk_write(thedisk,0,superBlock.data);
 
-	return 0;
+	return 1;
 }
 
 void fs_debug()
@@ -99,7 +102,7 @@ void fs_debug()
 		for (j = 0; j < INODES_PER_BLOCK; j++) {
 			struct fs_inode myInode = inodeBlock.inode[j];
 			if (!myInode.isvalid) continue;
-			printf("inode %d:\n", j + ((i-1) * INODES_PER_BLOCK));
+			printf("inode %d:\n", (j + 1 + ((i-1) * INODES_PER_BLOCK)));
 			printf("    size: %d bytes\n",myInode.size);
 			printf("    created: %s",ctime(&myInode.ctime));
 			// Loop through the direct pointer in each inode 
@@ -129,16 +132,110 @@ void fs_debug()
 
 int fs_mount()
 {
-	return 0;
+	int i, j, k;
+	if (mounted) {
+		return 0;
+	}
+
+	mounted = true;
+
+	// Read and verify superblock
+	union fs_block superBlock;
+	disk_read(thedisk,0,superBlock.data);
+	if ((superBlock.super.magic != FS_MAGIC) || superBlock.super.nblocks != disk_nblocks(thedisk) || superBlock.super.ninodes != (INODES_PER_BLOCK * superBlock.super.ninodeblocks)) {
+		return 0;
+	}
+	if (disk_nblocks(thedisk) % 10 == 0) {
+		if (superBlock.super.ninodeblocks != (disk_nblocks(thedisk) / 10)) return 0;
+	} else {
+		if (superBlock.super.ninodeblocks != (disk_nblocks(thedisk) / 10) + 1) return 0;
+	}
+	
+	// Build a free block bitmap
+	freeBlocks = calloc(superBlock.super.nblocks, sizeof(bool));
+
+	freeBlocks[0] = false;
+	for (i = 1; i < superBlock.super.nblocks; i++) {
+		freeBlocks[i] = true;
+	}
+
+	for (i = 1; i < superBlock.super.ninodeblocks; i++) {
+		union fs_block inodeBlock;
+		disk_read(thedisk, i, inodeBlock.data);
+
+		// Set inode block to not free
+		freeBlocks[i] = false;
+		
+		for (j = 0; i < INODES_PER_BLOCK; j++) {
+			struct fs_inode myInode = inodeBlock.inode[j];
+			if (!(myInode.isvalid)) continue;
+
+			// Make all the direct pointers not free
+			for (k = 0; k < POINTERS_PER_INODE; k++) {
+				if (!(myInode.direct[k])) continue;
+				freeBlocks[myInode.direct[k]] = false;
+			}
+
+			// Make indirect pointer block and blocks pointed to not free
+			if (myInode.indirect) {
+				freeBlocks[myInode.indirect] = false;
+				union fs_block indirectBlock;
+				disk_read(thedisk, myInode.indirect, indirectBlock.data);
+
+				for (k = 0; k < POINTERS_PER_BLOCK; k++) {
+					if (!(indirectBlock.pointers[k])) continue;
+					freeBlocks[indirectBlock.pointers[k]] = false;
+				}
+			}
+		}
+
+	}
+
+	return 1;
 }
 
 int fs_create()
 {
+	int i, j, k;
+	union fs_block superBlock;
+	disk_read(thedisk,0,superBlock.data);
+
+	// Loop through the inode blocks
+	for (i = 1; i <= superBlock.super.ninodeblocks; i++) {
+		union fs_block inodeBlock;
+		disk_read(thedisk,i,inodeBlock.data);
+
+		// Loop through each inode
+		for (j = 0; j < INODES_PER_BLOCK; j++) {
+			if (inodeBlock.inode[j].isvalid) {
+				continue;
+			}
+			inodeBlock.inode[j].isvalid = 1;
+
+			// Loop through each direct pointer and set to 0
+			for (k = 0; k < POINTERS_PER_INODE; k++) {
+				inodeBlock.inode[j].direct[k] = 0;
+			}
+			inodeBlock.inode[j].indirect = 0;
+			inodeBlock.inode[j].size = 0;
+			inodeBlock.inode[j].ctime = time(NULL);
+
+			disk_write(thedisk, i, inodeBlock.data);
+
+			return (j + 1 + ((i-1) * INODES_PER_BLOCK));
+		}
+	}
 	return 0;
 }
 
 int fs_delete( int inumber )
 {
+	union fs_block inodeBlock;
+	int32_t blockNum = (inumber / INODES_PER_BLOCK) + 1;
+	int32_t blockOffset = (inumber % INODES_PER_BLOCK) - 1;
+
+	disk_read(thedisk, blockNum, inodeBlock.data);
+
 	return 0;
 }
 
